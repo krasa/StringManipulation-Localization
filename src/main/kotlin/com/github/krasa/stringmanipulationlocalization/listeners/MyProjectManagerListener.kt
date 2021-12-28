71bf291bbe76c2e2bfe2ee8d21b4ae56fe27e128
GitHub Action <action@github.com>
2021-12-28 10:32:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/krasa/stringmanipulationlocalization/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/krasa/stringmanipulationlocalization/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.krasa.stringmanipulationlocalization.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.krasa.stringmanipulationlocalization.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/krasa/stringmanipulationlocalization/services/MyProjectService.kt
+++ src/main/kotlin/com/github/krasa/stringmanipulationlocalization/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.krasa.stringmanipulationlocalization.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.krasa.stringmanipulationlocalization.MyBundle
 
 class MyProjectService(project: Project) {
 
